7a0985e2e50f3889ce11234cfd7425fb799ddfc4
pedroca656 <pedroca656>
2018-06-20 18:32:40 -0300

    Commit inicial

---
+++ BusPoa/BusPoa/BusPoa/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BusPoa.Models;
+
+namespace BusPoa.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
